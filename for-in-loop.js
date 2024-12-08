91d4d2e5f6d293088acc664f299cd3cf9d05c698
Shailendra-Kumar-Pandey <shailendrakr2010@gmail.com>
2024-12-08 23:20:11 +0530

    practic object

--- for-in-loop.js
+++ for-in-loop.js
@@ -143,3 +143,126 @@ for (const number of addNumbers) {
   }
 }
 
+
+// for each loop using practic
+
+const numberArray = [1, 2, 3, 4, 5];
+
+numberArray.forEach(function (number) {
+    console.log(number);    
+});
+
+
+
+const fDatas = ['apple','banana','orenge'];
+
+fDatas.forEach(function(fData, index){
+    console.log(index, fData);
+});
+
+
+const colors = ["red", "green", "blue"];
+
+colors.forEach((color, index) => {
+  console.log(`${index}: ${color}`);
+});
+
+
+const firstNum =[1, 2, 3, 4, 5];
+
+firstNum.forEach(function(first, index, arr){
+    arr[index] = first * 2;
+})
+
+// console.log(firstNum);
+
+const data2 = firstNum;
+console.log(data2);
+
+
+// object function using example
+
+// const object = {
+//     key: value,  // regular property
+//     method: function() {  // function (method) as property
+//       // code to be executed
+//     }
+//   };
+  
+
+const class10 = {
+    firstName: 'Jeetu',
+    lastName: 'Pandey',
+    greet: function () {
+        console.log(`Hello, my name is ${this.firstName} ${this.lastName}`);        
+    }
+};
+
+class10.greet();
+
+
+const person88 = {
+    firstName: "Jane",
+    lastName: "Smith",
+    greet: () => {
+      console.log(`Hello, my name is ${this.firstName} ${this.lastName}`);
+    }
+  };
+  
+  person88.greet(); // Calling the method
+  
+
+  const class11 = {
+    firstName: 'Jeetu',
+    lastName: 'Pandey',
+    greet: function (greeting) {
+        console.log(`${greeting}, my name is ${this.firstName} ${this.lastName}`);        
+    }
+};
+
+class11.greet('Hi good mornig');
+
+
+const pandeyJi ={
+    name: 'Shailendra',
+    last: 'Pandey',
+    age: '28',
+    birthday: function(){
+        this.age++;
+        console.log(`Happy Birthday,${this.name} ${this.last}! You are now ${this.age} years old`);
+    }
+}
+
+pandeyJi.birthday();
+pandeyJi.birthday();
+
+
+
+const company ={
+    make: 'Toyota',
+    model: 'Camry',
+    year: 2010,
+    getCarInfo: function(){
+        return `${this.year} ${this.make} ${this.model}`;
+    }
+};
+
+console.log(company.getCarInfo());
+
+company.getCarInfo();
+
+
+function Person(firstName, lastName){
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.greet = function(){
+        console.log(`Hello, my name is ${this.firstName} ${this.lastName}`);
+    };    
+}
+
+const person3 = new Person ('Shailendra', 'Singh');
+const person4 = new Person('Gayatri', 'Sharma');
+
+person3.greet();
+person4.greet();
+console.log(person3);
